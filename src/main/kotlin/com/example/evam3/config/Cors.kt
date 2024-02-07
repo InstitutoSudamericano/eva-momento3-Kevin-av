1ad9754e7d21bd0920090c129cf6c37d14627721
Kevin-av <116099021+Kevin-av@users.noreply.github.com>
2024-02-07 15:28:32 -0500

    delete funcion

--- src/main/kotlin/com/example/evam3/config/Cors.kt
+++ src/main/kotlin/com/example/evam3/config/Cors.kt
@@ -2,19 +2,16 @@ package com.example.evam3.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 
 @Configuration
-@EnableWebMvc
-
-class Cors : WebMvcConfigurer{
+class CorsConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/")
-            .allowedOrigins(
-                "http://localhost:8082")
-            .allowedMethods("POST", "GET", "PUT", "DELETE", "post")
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:8082")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*")
             .allowCredentials(true)
     }
 }
--- src/main/kotlin/com/example/evam3/controller/FilmController.kt
+++ src/main/kotlin/com/example/evam3/controller/FilmController.kt
@@ -6,8 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.CrossOrigin
 
-@CrossOrigin(methods = [RequestMethod.GET, RequestMethod.POST, RequestMethod.PATCH, RequestMethod.PUT, RequestMethod.DELETE])
+@CrossOrigin(origins = ["http://localhost:8082"], methods = [RequestMethod.GET, RequestMethod.POST, RequestMethod.PATCH, RequestMethod.PUT, RequestMethod.DELETE])
 
 @RestController
 @RequestMapping("/film")
